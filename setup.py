373f5733c665a176758a0054bc140399517da436
ameya1984 <ameya1984@gmail.com>
2023-01-28 14:10:03 +0530

    Fixed dummy database setup

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from models import Base, Invoice, InvoiceItem
 from db import engine, get_session
 from datetime import datetime
+import namegenerator, random
 
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
-sess = get_session()
-sess.add_all([
-    Invoice(
-        date=datetime.now(),
-        items=[
-            InvoiceItem(
-                units=10,
-                description='invoiceItem description',
-                amount=99.99,
-            ) for j in range(5)])
-    for i in range(5)
-])
-sess.commit()
+session = get_session()
+
+for i in range(5):
+    invoice = Invoice(date=datetime.now())
+    for j in range(5):
+        item = InvoiceItem(
+            units=random.randrange(100),
+            description=namegenerator.gen(),
+            amount=round(random.uniform(0.99, 99.99), 2),
+            invoice=invoice
+        )
+        invoice.items.append(item)
+    session.add(invoice)
+
+session.commit()
